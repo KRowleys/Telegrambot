9db2bb6ba7c3ccebe43ed1c8c5b885c1b89dadbd
KRowleys <155213114+KRowleys@users.noreply.github.com>
2025-01-26 23:57:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // BOT TELE
 module.exports = {
-  BOT_TOKEN: "7644037071:AAE7EEapbxvy3Q9DVQFbOHT_d3xcGRSbGPE", // Token bot Telegram
+  BOT_TOKEN: "7261235427:AAEJRW9-YgSAHbURJtde7Kqd5nFnSI5G3uw", // Token bot Telegram
   OWNER_ID: "7516315928", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
